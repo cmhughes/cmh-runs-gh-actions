341e9f900beea01e88a6235fc483ee495382beb2
cmhughes <christopher.michael.hughes@gmail.com>
2022-10-22 18:00:50 +0100

    pdf working now...?

--- documentation/checklogfile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# check for undefined/multiply-defined references
-
-set -x
-egrep -i --color=auto 'undefined' latexindent.log
-egrep -i --color=auto 'multiply-defined' latexindent.log
-egrep -i --color=auto 'fixthis' latexindent.log
-exit
